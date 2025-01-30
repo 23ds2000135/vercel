529fcb6af72dff49ae81276bd97304ee64cfd8a9
23ds2000135 <23ds2000135@ds.study.iitm.ac.in>
2025-01-30 19:21:40 +0530

    3rd commit

--- api/index.py
+++ api/index.py
@@ -6,6 +6,7 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','application/json')
+        self.end_headers("Access-Control-Allow-Origin", "*")
         self.end_headers()
         self.wfile.write(json.dumps({"message": "Hello!"}).encode('utf-8'))
         return
